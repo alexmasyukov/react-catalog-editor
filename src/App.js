845351d6c2c62550207a42e7e55349a1aaf684be
alexmasukov <alienspro2008@yandex.ru>
2020-08-15 09:09:29 +0900

    prepare to fix key of rows

--- src/App.js
+++ src/App.js
@@ -43,55 +43,44 @@ const lastCid = Math.max(...categories.map(({ id = 0 }) => id))
 
 
 const data = {
-  // helpers: {
-  //   categoryIdMaker:
-  //   rowIdMaker: createCounter(lastRowsId + 1),
-  // },
   columns: [
     {
-      id: 20,
+      id: 1,
       title: 'ID',
       type: COLUMN_TYPES.ID,
-      idMaker: createCounter(lastCid + 1),
-      order: 1
-    },
-    {
-      id: 1,
-      title: 'CID',
-      type: COLUMN_TYPES.CATEGORY_ID,
-      idMaker: createCounter(lastRowsId + 1),
-      // hidden: true,
-      default: (cid) => cid,
+      name: 'id',
       order: 1
     },
     {
       id: 2,
-      title: 'Название',
-      type: COLUMN_TYPES.STRING,
+      title: 'CID',
+      type: COLUMN_TYPES.CATEGORY_ID,
+      name: 'cid',
+      // hidden: true,
       default: '',
       order: 2
     },
     {
       id: 3,
-      title: 'Цена',
-      type: COLUMN_TYPES.NUMBER,
-      default: 0,
+      title: 'Название',
+      type: COLUMN_TYPES.STRING,
+      name: 'name',
+      default: '',
       order: 3
     },
     {
-      id: 9,
-      title: 'Опубликовано',
-      type: COLUMN_TYPES.CHECK,
-      default: true,
-      style: {
-        textAlign: 'center'
-      },
+      id: 4,
+      title: 'Цена',
+      type: COLUMN_TYPES.NUMBER,
+      name: 'price',
+      default: 0,
       order: 4
     },
     {
-      id: 10,
-      title: 'В наличии',
+      id: 5,
+      title: 'Опубликовано',
       type: COLUMN_TYPES.CHECK,
+      name: 'public',
       default: true,
       style: {
         textAlign: 'center'
@@ -99,64 +88,82 @@ const data = {
       order: 5
     },
     {
-      id: 11,
-      title: 'Хит продаж',
+      id: 13,
+      title: 'В наличии',
       type: COLUMN_TYPES.CHECK,
-      default: false,
+      name: 'nalichiye',
+      default: true,
+      order: 6,
       style: {
         textAlign: 'center'
-      },
-      order: 6
+      }
     },
     {
-      id: 12,
+      id: 15,
+      title: 'Хит',
+      type: COLUMN_TYPES.CHECK,
+      name: 'khit',
+      default: false,
+      order: 7,
+      style: {
+        textAlign: 'center'
+      }
+    },
+    {
+      id: 14,
       title: 'Новинка',
       type: COLUMN_TYPES.CHECK,
+      name: 'novinka',
       default: false,
+      order: 8,
       style: {
         textAlign: 'center'
-      },
-      order: 7
+      }
     },
     {
-      id: 4,
+      id: 16,
+      title: 'Товар недели',
+      type: COLUMN_TYPES.CHECK,
+      name: 'tovar--nedeli',
+      default: false,
+      order: 9
+    },
+    {
+      id: 11,
       title: 'Скидка %',
       type: COLUMN_TYPES.NUMBER,
-      default: '12'
-    },
-    {
-      id: 5,
-      title: 'Цена со скидкой',
-      type: COLUMN_TYPES.NUMBER,
-      default: ''
-    },
-    {
-      id: 7,
-      title: 'Измеряется',
-      type: COLUMN_TYPES.STRING,
-      default: 'шт',
-      order: 8
+      name: 'skidka',
+      default: '12',
+      order: 10
     },
     {
       id: 6,
       title: 'Описание',
       type: COLUMN_TYPES.TEXT,
-      default: ''
+      name: 'description',
+      default: '',
+      order: 11
     },
     {
       id: 30,
       title: 'Фото',
       type: COLUMN_TYPES.IMAGES,
-      getImage: (img) => `https://xn--80aeff2a2ak7at8e.xn--p1ai/sync/${img}`,
-      uploadImageUrl: 'https://xn--80aeff2a2ak7at8e.xn--p1ai/cms/pages/db_orders/get_img_test.php',
       default: [
         ['small_27_03_2020_09_08_53_779J6zxr.jpg', '27_03_2020_09_08_53_779J6zxr.jpg'],
         ['small_10_03_2020_11_28_42_172NfmB6.jpg', '10_03_2020_11_28_42_172NfmB6.jpg']
-      ]
+      ],
+      order: 12
     }
   ],
   categories,
   rows
+}
+
+const config = {
+  idCounter: createCounter(lastRowsId + 1),
+  cidCounter: createCounter(lastCid + 1),
+  getImage: (img) => `https://xn--80aeff2a2ak7at8e.xn--p1ai/sync/${img}`,
+  uploadImageUrl: 'https://xn--80aeff2a2ak7at8e.xn--p1ai/cms/pages/db_orders/get_img_test.php'
 }
 
 function App() {
@@ -167,6 +174,7 @@ function App() {
   return (
      <div className="App">
        <Editor
+          config={config}
           data={data}
           onChange={handleOnChange}/>
      </div>
